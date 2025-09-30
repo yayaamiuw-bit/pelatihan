8cd6f951dd52986d0aa6473437e85d17abfc359a
yayaamiuw-bit <fayadwinovitasari@gmail.com>
2025-09-30 14:14:18 +0700

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,1 +1,7 @@
-
+// 1. Pilih tombol berdasarkan ID-nya
+const tombolSapa = document.getElementById('sapaButton');
+// 2. Tambahkan 'event listener' untuk aksi 'click'
+tombolSapa.addEventListener('click', function() {
+  // 3. Jalankan kode ini ketika tombol di-klik
+  alert('Halo! Terima kasih sudah berkunjung!');
+});
